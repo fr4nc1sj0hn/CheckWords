9249ece2cd48fa4d279c6ba28b18ef3865954882
INTERNAL\FRANC583 <FRANCIS.JOHN.PICASO@willistowerswatson.com>
2018-03-25 08:05:52 +0800

    Add project files.

---
+++ CheckWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
